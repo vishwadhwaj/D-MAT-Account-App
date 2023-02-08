f1288792a7bc75c227a4669093cc3d3e15698115
Vishwadhwaj <kumarvishwadhwaj@gmail.com>
2023-02-08 15:36:18 +0530

    Initial Commit

---
+++ d-mat-account/src/main/java/com/vishwadhwaj/d_mat_account/App.java
@@ -0,0 +1,13 @@
+package com.vishwadhwaj.d_mat_account;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
