c12178cb904dcef9c2aa94f41ceb520191dfa18e
Vishwadhwaj <kumarvishwadhwaj@gmail.com>
2023-02-08 19:22:18 +0530

    test database connection

--- d-mat-account/src/main/java/com/vishwadhwaj/d_mat_account/App.java
+++ d-mat-account/src/main/java/com/vishwadhwaj/d_mat_account/App.java
@@ -1,13 +1,30 @@
 package com.vishwadhwaj.d_mat_account;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.vishwadhwaj.d_mat_account.db.Db;
+
+public class App {
+	public static void main(String[] args) {
+		Db db = Db.getInstance();
+		Connection connection = db.createConnection();
+		PreparedStatement statement;
+		try {
+			statement = connection.prepareStatement("insert into share (name,value) values ('xyz','2000')");
+			statement.executeUpdate();
+			System.out.println("command executed succesfully");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
