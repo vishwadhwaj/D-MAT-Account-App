1ec02e15f4133192e5cb865a2bb1ef4285891633
Vishwadhwaj <kumarvishwadhwaj@gmail.com>
2023-02-08 21:11:50 +0530

    initial app code

--- d-mat-account/src/main/java/com/vishwadhwaj/d_mat_account/App.java
+++ d-mat-account/src/main/java/com/vishwadhwaj/d_mat_account/App.java
@@ -1,30 +1,52 @@
 package com.vishwadhwaj.d_mat_account;
+import java.util.*;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import com.vishwadhwaj.d_mat_account.db.Db;
 
 public class App {
+	
+	private App() {
+		
+	}
+	
+	public static void showMainMenu() {
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("Welcome to the D-Mat Account");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println();
+		System.out.println();
+		boolean flag=true;
+		while(flag) {
+			System.out.println("1.Create Account");
+			System.out.println("2.Login");
+			System.out.println("3.quit");
+			try (Scanner scanner = new Scanner(System.in)) {
+				int choice=scanner.nextInt();
+				switch(choice){
+					case 1:
+						showRegistrationForm();
+						break;
+					case 2:
+						showLoginForm();
+						break;
+					case 3:
+						flag=false;
+						break;
+					default:
+						System.out.println("Enter Valid Choice");
+				}
+			}
+			
+		}
+		
+	}
+	public static void showRegistrationForm() {
+		
+	}
+	public static void showLoginForm() {
+		
+	}
 	public static void main(String[] args) {
-		Db db = Db.getInstance();
-		Connection connection = db.createConnection();
-		PreparedStatement statement;
-		try {
-			statement = connection.prepareStatement("insert into share (name,value) values ('xyz','2000')");
-			statement.executeUpdate();
-			System.out.println("command executed succesfully");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			connection.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+		
+		showMainMenu();
 	}
 }
