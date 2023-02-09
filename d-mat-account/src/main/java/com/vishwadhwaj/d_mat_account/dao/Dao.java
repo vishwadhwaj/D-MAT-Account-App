a8edb6cc61a85e8bff5c464f016e1f94020a4bbe
Vishwadhwaj <kumarvishwadhwaj@gmail.com>
2023-02-09 18:26:52 +0530

    change return type of methods

--- d-mat-account/src/main/java/com/vishwadhwaj/d_mat_account/dao/Dao.java
+++ d-mat-account/src/main/java/com/vishwadhwaj/d_mat_account/dao/Dao.java
@@ -3,8 +3,8 @@ package com.vishwadhwaj.d_mat_account.dao;
 import java.util.List;
 
 public interface Dao<T> {
-	public T create(T object);
-	public T update(T object);
-	public T delete(T object);
+	public boolean create(T object);
+	public boolean update(T object);
+	public boolean delete(T object);
 	public List<T> read();
 }
