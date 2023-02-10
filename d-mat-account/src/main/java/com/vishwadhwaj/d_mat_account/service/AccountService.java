3e0d54226a5549661e56328b812cb95a301d462c
Vishwadhwaj <kumarvishwadhwaj@gmail.com>
2023-02-10 11:30:52 +0530

    create initial structure of transaction and account service

---
+++ d-mat-account/src/main/java/com/vishwadhwaj/d_mat_account/service/AccountService.java
@@ -0,0 +1,26 @@
+package com.vishwadhwaj.d_mat_account.service;
+
+public class AccountService {
+
+	private static AccountService accountService=new AccountService();
+	
+	private AccountService() {
+		
+	}
+	
+	public static AccountService getInstance() {
+		return accountService;
+	}
+	
+	public void showAccountDetails() {
+		
+	}
+	
+	public void depositMoney() {
+		
+	}
+	
+	public void withdrawMoney() {
+		
+	}
+}
---
+++ d-mat-account/src/main/java/com/vishwadhwaj/d_mat_account/service/TransactionService.java
@@ -0,0 +1,26 @@
+package com.vishwadhwaj.d_mat_account.service;
+
+public class TransactionService {
+
+	private static TransactionService transactionService=new TransactionService();
+	
+	private TransactionService() {
+		
+	}
+	
+	public static TransactionService getInstance() {
+		return transactionService;
+	}
+	
+	public void buyTransaction() {
+		
+	}
+	
+	public void sellTransaction() {
+		
+	}
+	
+	public void viewTransactionReport() {
+		
+	}
+}
